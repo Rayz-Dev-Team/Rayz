0e31520a2022d18b323215e99255756988738a14
YumYummity <103061664+YumYummity@users.noreply.github.com>
2022-11-04 11:13:06 -0400

    ctx.reply and minor changes
    Using ctx.reply instead of ctx.send for a cleaner experience.
    Line 27 in startup, now prints user it logged into.

--- base.py
+++ base.py
@@ -1,16 +1,12 @@
 import guilded
 from guilded.ext import commands
-import random
 import json
 import glob
 import sys
 import os
-from tools.dataIO import fileIO
 from core import checks
 from core import prefix
 import logging
-import psycopg2
-from psycopg2 import Error
 
 logger = logging.getLogger('guilded')
 logger.setLevel(logging.DEBUG)
@@ -28,7 +24,7 @@ bot = commands.Bot(bot_id="acd5fc8c-4272-48d0-b78b-da1fecb1bab5", command_prefix
 
 @bot.event
 async def on_ready():
-    print("[ Online and connected to Guilded. ]")
+    print(f"[ Online and connected to Guilded. User: {bot.user}]")
     print('------')
 
 @bot.event
@@ -37,7 +33,7 @@ async def on_command_error(ctx, error):
         return
     else:
         try:
-            em = guilded.Embed(title="An error has occured trying to process your command.", description="`-` Screenshot this, and [send it to the developer](https://www.guilded.gg/i/E0LaMb4E)\n\n{}".format(error), color=0x363942)
+            em = guilded.Embed(title="An error occured while trying to process your command.", description="`-` Screenshot this, and [send it to the developer](https://www.guilded.gg/i/E0LaMb4E)\n\n{}".format(error), color=0x363942)
             await ctx.reply(private=True, embed=em)
         except:
             print(error)
@@ -50,12 +46,12 @@ async def load(ctx, *, cog_name: str):
     try:
         bot.load_extension(cog_name)
     except Exception as e:
-        em = guilded.Embed(description="Failed to load the Module.", color=0x363942)
-        await ctx.send(embed=em)
+        em = guilded.Embed(description="Failed to load the module.", color=0x363942)
+        await ctx.reply(embed=em)
         print('{}: {}'.format(type(e), e))
     else:
         em = guilded.Embed(description="**Module loaded.** :)", color=0x363942)
-        await ctx.send(embed=em)
+        await ctx.reply(embed=em)
 
 @bot.command()
 @checks.is_dev()
@@ -66,17 +62,17 @@ async def unload(ctx, *, cog_name: str):
     if cog_name in bot.extensions:
         bot.unload_extension(cog_name)
         em = guilded.Embed(description="**Module unloaded.** :)", color=0x363942)
-        await ctx.send(embed=em)
+        await ctx.reply(embed=em)
     else:
-        em = guilded.Embed(description="That Module isn't loaded.", color=0x363942)
-        await ctx.send(embed=em)
+        em = guilded.Embed(description="That module isn't loaded.", color=0x363942)
+        await ctx.reply(embed=em)
 
 @bot.command()
 @checks.is_dev()
 async def reload(ctx, *, cog_name: str = None):
     if cog_name == None:
         embed = guilded.Embed(description="Reloading... ('thank you not here, you are amazing, I love you' - Beezo)",colour=0x363942)
-        await ctx.send(embed = embed)
+        await ctx.reply(embed = embed)
         os.execv(sys.executable, ['python'] + sys.argv)
     else:
         if not cog_name.startswith("modules."):
@@ -85,12 +81,12 @@ async def reload(ctx, *, cog_name: str = None):
             bot.unload_extension(cog_name)
             bot.load_extension(cog_name)
         except Exception as e:
-            em = guilded.Embed(description="Failed to reload the Module.", color=0x363942)
-            await ctx.send(embed=em)
+            em = guilded.Embed(description="Failed to reload the module.", color=0x363942)
+            await ctx.reply(embed=em)
             print('{}: {}'.format(type(e), e))
         else:
             em = guilded.Embed(description="**Module reloaded.** :)", color=0x363942)
-            await ctx.send(embed=em)
+            await ctx.reply(embed=em)
 
 if __name__ == '__main__':
     for thing in startup:
