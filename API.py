93b1f991b8b20950d84649283b9aadafbc99e1fd
Beezo <peython2017@hotmail.com>
2023-03-05 12:16:59 -0800

    [API] CORS implimentation, and fix in displaying bot data

--- API.py
+++ API.py
@@ -1,4 +1,4 @@
-from quart import Quart, request
+from quart import Quart, request, send_file, Response, jsonify, url_for
 import quart
 import os
 import psycopg
@@ -10,8 +10,12 @@ from psycopg.rows import dict_row
 from core.database import *
 from functools import wraps
 import requests
+from quart_cors import cors, route_cors
+
 
 app = Quart(__name__)
+app = cors(app, allow_origin=["*"], allow_headers="*")
+
 
 def token_required(f):
     @wraps(f)
@@ -38,13 +42,44 @@ def token_required(f):
             else:
                 return {'message': 'UserID does not exist.'}, 401
     return decorated
-    
 
 @app.route('/')
+@route_cors(allow_origin="*")
 async def index():
     return 'Turn back while you still can.'
 
+@app.route("/capoo")
+@route_cors(allow_headers=["content-type"], allow_origin="*")
+async def Capoo():
+    with open("C:/Users/SgtZo/Desktop/Capoo/Capoo_squish.gif", "rb") as f:
+        image_data = f.read()
+    response = quart.Response(image_data, mimetype="image/gif")
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
+
+@app.route("/pack/<pack_name>", methods=["GET"])
+@route_cors(allow_headers=["content-type"], allow_methods=["GET"], allow_origin="*")
+async def GetPack(pack_name):
+    if pack_name == "capoo":
+        image_path = 'C:/Users/SgtZo/Desktop/Capoo/Capoo_squish.gif'
+        image_url = url_for('Capoo', _external=True)
+
+        capoo_pack = {
+            "name": "Capoo Emoji Pack",
+            "author": "Rayz API",
+            "emotes": [
+                {
+                    "name": "capoo_cry",
+                    "url": image_url
+                }
+            ]
+        }
+        response = quart.Response(json.dumps(capoo_pack), mimetype='application/json')
+        response.headers.add("Access-Control-Allow-Origin", "*")
+        return response
+
 @app.route("/<server_id>/staff", methods=["GET"])
+@route_cors(allow_headers=["content-type"], allow_methods=["GET"], allow_origin="*")
 async def GetStaffMembers(server_id):
     staff_member_id_list = {}
     req_roles = requests.get("https://www.guilded.gg/api/teams/{}/info".format(server_id))
@@ -70,14 +105,17 @@ async def GetStaffMembers(server_id):
                             i["profilePicture"] = "https://imgur.com/RGYNw2v"
                         if "profileBannerBlur" not in i:
                             i["profileBannerBlur"] = 404
-                        staff_member_id_list[i["id"]] = {
-                            "avatar": '{}'.format(i["profilePicture"]),
-                            "name": '{}'.format(i["name"]),
-                            "id": '{}'.format(i["id"]),
-                            "banner": '{}'.format(i["profileBannerBlur"])
-                        }
+                        if "type" not in i:
+                            staff_member_id_list[i["id"]] = {
+                                "avatar": '{}'.format(i["profilePicture"]),
+                                "name": '{}'.format(i["name"]),
+                                "id": '{}'.format(i["id"]),
+                                "banner": '{}'.format(i["profileBannerBlur"])
+                            }
     j = json.dumps(staff_member_id_list)
-    return quart.Response(j, mimetype="application/json")
+    response = quart.Response(j, mimetype="application/json")
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
 
 
 @app.route("/usercount", methods=["GET"])
