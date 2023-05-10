001256bf6b468bcdb144536bbceb7f1dd6b16b0f
Beezo <peython2017@hotmail.com>
2023-05-10 13:51:25 -0700

    test

--- modules/economy.py
+++ modules/economy.py
@@ -574,11 +574,11 @@ class Economy(commands.Cog):
 		item_names = []
 
 		for i in item_list:
-			print(item_list[i]["data"]["display_name"].lower())
-			display_item_names[item_list[i]["data"]["display_name"].lower()] = {
-				"name": item_list[i]["item"]
+			print(i["data"]["display_name"].lower())
+			display_item_names[i["data"]["display_name"].lower()] = {
+				"name": i["item"]
 			}
-			item_names.append(item_list[i]["item"])
+			item_names.append(i["item"])
 
 		if author.id in LB_bans["bans"]:
 			em = guilded.Embed(title="Uh oh!", description="You were banned from Rayz's Economy for violating our ToS.", color=0x363942)
